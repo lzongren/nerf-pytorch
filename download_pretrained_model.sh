8a2a9e5860991a83b20d069c10dbdbd2bb4abf00
lzongren <lzongren@gmail.com>
2024-04-06 22:01:58 +0200

    Create a script for downloading pre-trained model

---
+++ download_pretrained_model.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# prepare pre-trained model folder
+echo "Prepare pre-trained model folders"
+mkdir -p logs
+cd logs
+
+# download pre-trained models
+# https://drive.google.com/drive/folders/1jIr8dkvefrQmv737fFm2isiT6tqpbTbv
+
+# download and place flower_test
+echo "Download flower_test pre-trained model"
+rm -rf flower_test
+gdown --folder https://drive.google.com/drive/folders/1573Sa1xlNh-F7jSQa-XB858KvDjpe2xA
+mv flower_test logs/
+
+# download and place fern_test
+echo "Download fern_test pre-trained model"
+!rm -rf fern_test
+! gdown --folder https://drive.google.com/drive/folders/1PIWs8aeT48FHORaF8E7vKc0KyVKJ1Hlg
+!mv fern_test logs/
